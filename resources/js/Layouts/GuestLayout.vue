8ea9448b21127be36c4a63454ba9a70983ff71e8
Sean Mueller <sem@delife.eu>
2023-06-05 13:48:56 +0200

    New: StarterKit Breeze
    Ich hab nach mehreren Fehlschlägen das Starterkit Breeze zum einbinden von
    Vue verwendet :D

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Navi from '../Components/Navi.vue';
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Navi></Navi>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
+        
+
+        <div
+            class="w-full sm:max-w-md  px-6 py-4  bg-neutral-500 overflow-hidden sm:rounded-lg"
+        >
+            <slot />
+        </div>
+    </div>
+</template>
